fe05b2891c3977e706eee524096dfee096c6a683
daniel-cambium <ldaniel@cambium.co.il>
2014-05-30 10:56:45 +0300

    add html2canvas

---
+++ data/getImage.php
@@ -0,0 +1,6 @@
+<?php
+$data=json_decode(file_get_contents('php://input'));
+$url='img/'. sha1( microtime()).'.png';
+file_put_contents($url, base64_decode($data->imgBase64));
+
+echo "{'url':'".$url."'}";
